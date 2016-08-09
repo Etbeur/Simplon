a23e8018367b9ffaf27e0663138dad08fe3a4c13
etbeur <romain.etheve@gmail.com>
2016-08-09 16:57:30 +0200

    Maj minikata_tableau

--- aout/semaine5/minikata_tableau.js
+++ aout/semaine5/minikata_tableau.js
@@ -1,76 +1,109 @@
 var prenom = "paul";
 
 // TODO : ajouter une majuscule
-/*
-    ?
-*/
+prenom = prenom.charAt(0).toUpperCase() + prenom.substring(1).toLowerCase();
 
-console.log('prenom avec premiere lettre en majuscule', /* ? */ ); //Paul
+console.log('prenom avec premiere lettre en majuscule', prenom); //Paul
 
 var users = ["joe", "lea", "bob", "ann"];
 
 // TODO : ajouter une majuscule à tous les prenoms de la liste
-console.log('Prenoms avec premiere lettre en majuscule', /* ? */ ); //Joe, Lea, Bob, Ann
+var newUsers = [];
+for (var i = 0; i < users.length; i++){
+  newUsers += users[i].charAt(0).toUpperCase() + users[i].slice(1).toLowerCase() + " ";
+}
+console.log('Prenoms avec premiere lettre en majuscule', newUsers); //Joe, Lea, Bob, Ann
 
 // TODO : liste des prenoms qui commencent par une voyelle
-
 // TODO : liste des prenoms qui commencent par une console
+var voyelles = "aeiouy";
+var prenomVoyelles = [];
+var prenomConsonne = [];
+for (var i = 0; i < users.length; i++){
+  if(voyelles.indexOf(users[i].charAt(0))){
+    prenomVoyelles.push(users[i]);
+    console.log(prenomVoyelles);
+  }else{
+    prenomConsonne.push(users[i]);
+    console.log(prenomConsonne);
+  }
+}
 
 
 var notes = [10, 12, 13, 4, 8];
-
+somme = 0;
 // TODO : calcul de la somme
-/*
- ?
- */
-console.log('somme des notes', /* ? */);
+for(var i = 0; i < notes.length; i++){
+  somme += notes[i];
+}
+
+
+console.log('somme des notes', somme);
+
 
 // TODO : calcul de la moyenne
-/*
-?
-*/
-console.log('moyenne', /* ? */);
+moyenne = 0;
+somme = 0;
+for(var i = 0; i < notes.length; i++){
+  somme += notes[i];
+  moyenne = somme/notes.length;
+}
+console.log('moyenne', moyenne );
+
 
 var nouvelleNote = 12;
-
 // TODO : ajouter la note à liste
+notes.push(nouvelleNote);
+console.log(notes);
+
 
 // TODO : calcul de la nouvelle somme
-/*
- ?
- */
-console.log('somme des notes', /* ? */);
+somme += nouvelleNote;
+console.log('somme des notes', somme);
+
 
 // TODO : calcul de la nouvelle moyenne
-/*
- ?
- */
-console.log('moyenne', /* ? */);
+moyenne = somme/notes.length;
+console.log('moyenne',moyenne );
+
 
 // TODO : trouvez la note la plus haute
-console.log('meilleure note', /* ? */);
+var bonneNote = 0;
+for(var i = 0; i < notes.length; i++){
+  if(notes[i] > bonneNote){
+    bonneNote = notes[i];
+  }
+}
+console.log('meilleure note', bonneNote );
 
-// TODO : trouvez la note la plus haute
-console.log('plus mauvaise note', /* ? */);
+// TODO : trouvez la note la plus basse
+var mauvaiseNote = 20;
+for(var i = 0; i < notes.length; i++){
+  if(notes[i] < mauvaiseNote){
+    mauvaiseNote = notes[i];
+  }
+}
+
+console.log('plus mauvaise note', mauvaiseNote);
 
 
 // TODO : passage en fonctions
 function calculSomme( tableauDeNotes ){
-    var somme = 0;
-
-    /*
-    ?
-     */
-
+  var somme = 0;
+  for(var i = 0; i < tableauDeNotes.length; i++){
+    somme += tableauDeNotes[i];
+  }
     return somme;
 }
 
 function calculMoyenne( tableauDeNotes ){
-    var moyenne;
-
-    /*
-    ?
-     */
-
+  var moyenne;
+  var somme = 0;
+  for(var i = 0; i < tableauDeNotes.length; i++){
+    somme += tableauDeNotes[i];
+    moyenne = somme/tableauDeNotes.length;
+  }
     return moyenne;
 }
+console.log(calculSomme(notes));
+console.log(calculMoyenne(notes));
