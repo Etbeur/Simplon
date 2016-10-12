57639817cad45e90f6e0d936960e9c6412859271
etbeur <romain.etheve@gmail.com>
2016-10-12 12:28:39 +0200

    modif memo html

---
+++ septembre/semaine12/Objets/blog.js
@@ -0,0 +1,12 @@
+var blog = {
+// Creation du blog
+  ajouteArticle:function(titre, date, contenu, mailAuteur){
+    console.log('nouvel ajout');
+    var article = {
+      titre: titre,
+      date: date,
+      contenu: contenu,
+      mailAuteur: mailAuteur
+    };
+  }
+};
